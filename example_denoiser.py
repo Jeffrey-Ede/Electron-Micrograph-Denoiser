80c3e9091a267a826dea1046733677001a1c0abe
Jeffrey Ede <jeffreyede1@outlook.com>
2020-10-20 16:28:52 +0100

    More clearly provide saved checkpoint location

--- example_denoiser.py
+++ example_denoiser.py
@@ -5,8 +5,11 @@ from denoiser import Denoiser, disp
 #Try replacing this with your own image!
 img = np.random.rand(1500, 1500)
 
+#Replace with the location of your saved checkpoint
+checkpoint_loc = "//flexo.ads.warwick.ac.uk/Shared41/Microscopy/Jeffrey-Ede/models/denoiser-multi-gpu-13/model"
+
 #Initialize the denoising neural network
-noise_remover = Denoiser(visible_cuda="0")
+noise_remover = Denoiser(checkpoint_loc=checkpoint_loc, visible_cuda="0")
 
 #Denoise a 512x512 crop from the image
 crop = img[:512,:512]
